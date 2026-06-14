d5831f08b2fad665a10a2e4d36b7bc1c81646f94
2025jaspreetchalotra-creator <2025.jaspreet.chalotra@ves.ac.in>
2026-06-14 16:46:13 +0530

    added start command in package json

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const cors = require('cors')
 
 const app=express()
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(cors())
 app.use(morgan('dev'))
 
 //middleWare
